5aadd90c24c2d17d48027d701bf53a056d72346f
Sunxz007 <977622085@qq.com>
2019-06-11 08:49:00 +0800

    补充哈希表算法

--- js/Dictionary.js
+++ js/Dictionary.js
@@ -60,5 +60,76 @@ var HashTable = function () {
     this.getItems=function(){
         return items;
     }
+}
 
+var HashTable2=function(){
+    var table=[];
+    var loseloseHashCode = function (key, value) {
+        var hash = 0;
+        for (var i = 0; i < key.length; i++) {
+            hash += key[i].charCodeAt();
+        }
+        // 这里除以素数37是为了降低计算出重复hash的概率（后续会处理hash重复的问题）
+        return hash % 37;
+    }
+    var Node=function(key,value){
+        this.key=key;
+        this.value=value;
+    }
+    this.put=function(key,value){
+        var position=loseloseHashCode(key);
+        if(table[position]){
+            table[position].append(new Node(key,value));
+        }else{
+            var l=new LinkedList();
+            table[position]=l;
+            table[position].append(new Node(key,value));
+        }
+
+    }
+    this.get=function(key){
+        var position=loseloseHashCode(key);
+        if(table[position]){
+
+            //链表属性查找
+            var current=table[position].getHead();
+            while(current){
+                if(current.item.key==key){
+                    return current.item.value;
+                }
+                current=current.next;
+            }
+        }else{
+            return undefined;
+        }
+    }
+
+    this.remove=function(key){
+        var position=loseloseHashCode(key);
+        if(table[position]){
+            var previous=null;
+            var current=table[position].getHead();
+            
+            while(current.next){
+                if(current.item.key==key){
+                    table[position].remove(current.item)
+                    return true;
+                }
+                current=current.next
+            }
+            if(current.item.key==key){
+                table[position].remove(current.item)
+                if(table[position].isEmpty()){
+                    table[position]=undefined;
+                }
+                return true;
+            }
+            return false;
+        }else{
+            return false;
+        }
+    }
+    this.getTable=function(){
+        return table;
+    }
 }
